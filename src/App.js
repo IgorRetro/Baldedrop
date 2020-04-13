ba8f035a8cb7ce50b2fd1bb587f3fa8ec5359fde
Igor Chuglazov <igor.chuglazov@gmail.com>
2020-04-13 19:48:02 +0300

    new

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+const header = () => (
+  <div>
+    <img alt='logo' />
+  </div>
+)
+const Navbar = () => (
+  <div>
+
+  </div>
+)
+const main = () => (
+  <div>
+
+  </div>
+)
+const App = () => (
+  <div className='App-wrapper'>
+    <header />
+    <Navbar />
+    <main />
+  </div>
+);
+
+
+export default App;
