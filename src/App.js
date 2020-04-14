b5d260402d3ecc81ebe0f4091a6d9dd5669a0e61
Igor Chuglazov <igor.chuglazov@gmail.com>
2020-04-14 20:29:51 +0300

    landing page changed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import './App.css';
+import LoginModal from './LoginModal'
 
-const header = () => (
+const Header = () => (
+  <header className="header">
+    <button className="logo">Baldedrop</button>
+    <ul className="nav-links">
+      <li><button className="header-button">Plan</button></li>
+      <li><button className="header-button">Security</button></li>
+      <li><button className="header-button">Features</button></li>
+      <li><button className="header-button">About us</button></li>
+    </ul>
+  </header>
+
+)
+
+const Main = () => (
   <div>
-    <img alt='logo' />
+    <div className="background">
+      <div className="application-highlight">
+        <h1>Welcome to our data storage
+        for your easy teamwork!</h1>
+      </div>
+      <LoginModal email="Email" password="Password" rememberMe="true" />
+
+    </div>
+    <div className="features">
+      <h2>
+        Features
+      </h2>
+    </div>
   </div>
 )
-const Navbar = () => (
-  <div>
 
-  </div>
-)
-const main = () => (
-  <div>
-
-  </div>
-)
 const App = () => (
   <div className='App-wrapper'>
-    <header />
-    <Navbar />
-    <main />
+    <Header />
+    <Main />
   </div>
 );
 
---
+++ src/LoginModal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoginModal.css';
+
+const LoginModal = (email, password, rememberMe) => (
+    <div className="login-modal">
+
+    </div>
+)
+
+export default LoginModal;
